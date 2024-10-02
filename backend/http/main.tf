260bb57468d7a66ca9886849a4e8c83395497b6f
jayamorin <jay.amorin@gmail.com>
2024-10-02 20:22:26 +0800

    Qoute variables.

--- backend/http/main.tf
+++ backend/http/main.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "http" {
-    address = ${var.address}
+    address = "${var.address}"
     lock_address = "${var.lock_address}"
     unlock_address = "${var.unlock_address}"
-    username = ${var.username}
-    password = ${var.password}
-    lock_method = ${var.lock_method}
-    unlock_method = ${var.billing_email}
+    username = "${var.username}"
+    password = "${var.password}"
+    lock_method = "${var.lock_method}"
+    unlock_method = "${var.unlock_method}"
     retry_wait_min = ${var.retry_wait_min}
   }
 }
