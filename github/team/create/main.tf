a974e74b80c62f29baa4807d76d11f80f6690046
jayamorin <jay.amorin@gmail.com>
2024-10-09 01:00:41 +0800

    Remove dynamic lifecycle blolck not supported in github_team.

--- github/team/create/main.tf
+++ github/team/create/main.tf
@@ -3,13 +3,4 @@ resource "github_team" "team" {
   description               = var.description
   privacy                   = var.privacy
   create_default_maintainer = var.create_default_maintainer
-
-  dynamic "lifecycle" {
-    for_each = var.prevent_destroy ? [1] : []
-
-    content {
-      prevent_destroy = true
-    }
-  }
 }
-
